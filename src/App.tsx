eaa3b884dacf492e963aa55e0a646fad64306888
Don Vicente <m.vicent@gmail.com>
2020-02-25 17:53:31 -0300

    Tab Setup.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,14 @@ import TabsView from "./TabsView/TabsView";
 function App() {
   return (
       <div>
-        <TabsView tabs={[1,2,3]} />
+        <TabsView selected={0}
+                  tabs={[
+                      {title: "Tab 1", content:<div>a simple div</div>},
+                      {title: "Tab 2", content:<div>a more complex div</div>},
+                      {title: "Tab 3, title", content:<div><img src="https://lh3.googleusercontent.com/proxy/pJnP-TmyUXYHDRzrlo2SjY7-e11IhusDrgWqsGQcVIB9phgbuWlHzvQPFQNi0bf2vxRnlZwyoADghp-uqxTGyn1D7e_pDViVc_ovAFD4RCQ35SKHTY6lcnvezY3ZgtMdnVVA8hw"/></div>},
+                  ]}
+
+        />
         <div className="box">
           <div className="tabs">
             <div className="tab selected">Tab 4</div>
